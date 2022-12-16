f8e0f9fd5f424a0bdfc2f98162f0b8251d4244c2
HJeong1200 <hjeong1200@gmail.com>
2022-12-16 13:19:26 +0900

    feat: 새로운 할일 타이핑 기능

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,5 +1,21 @@
+import { useState } from 'react';
+
 function TodoList() {
-  return <div className="Todolist_Container">TodoList</div>;
+  const [newTodo, setNewTodo] = useState('');
+
+  const inputNewTodo = function (e) {
+    const text = e.target.value;
+    setNewTodo(text);
+  };
+
+  console.log(newTodo);
+
+  return (
+    <div className="Todolist_Container">
+      <h1 className="Todolist_Header">⭐️ Todo List</h1>
+      <input type="text" onChange={inputNewTodo} />
+    </div>
+  );
 }
 
 export default TodoList;
