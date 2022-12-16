f4d856172735fac00956070aeb21bc3c36037fb0
HJeong1200 <hjeong1200@gmail.com>
2022-12-16 14:34:27 +0900

    feat: 생성한 투두 수정 기능 구현

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,14 +1,40 @@
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 
 function Todo({ todoObj, todoList, setTodoList }) {
   const { id, todo } = todoObj;
+  const [isEditing, setIsEditing] = useState(false);
+  const [newTodo, setNewTodo] = useState(todo);
+
   const deleteTodo = function () {
     setTodoList(todoList.filter((todoObj) => todoObj.id !== id));
   };
 
+  const editTodo = function () {
+    setIsEditing(() => !isEditing);
+
+    if (isEditing) {
+      console.log(newTodo);
+      setTodoList(
+        todoList.map((todoObj) => {
+          if (todoObj.id === id) return { ...todoObj, todo: newTodo };
+          return todoObj;
+        })
+      );
+    }
+  };
+
+  const changeTodo = function (e) {
+    const text = e.target.value;
+    setNewTodo(text);
+  };
+
   return (
     <li>
-      <div className="Todo_Content">{todo}</div>
+      <div className="Todo_Content">
+        {isEditing ? <input value={newTodo} onChange={changeTodo} /> : todo}
+      </div>
+      <button onClick={editTodo}>E</button>
       <button onClick={deleteTodo}>X</button>
     </li>
   );
