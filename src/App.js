224d67c37846fa89d8a5e7831387fad2d676460e
HJeong1200 <hjeong1200@gmail.com>
2022-12-16 13:08:25 +0900

    feat: Todolist와 App 컴포넌트 연결

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import TodoList from './components/TodoList';
+
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <>
+      <div className="App">Hello</div>
+      <TodoList />
+    </>
+  );
 }
 
 export default App;
